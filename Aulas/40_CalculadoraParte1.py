694e9c77ce8b68d5d24cf71c98e0ee4a5d109acd
Otavio Luiz <95648653+TavinhoNZT@users.noreply.github.com>
2024-01-17 16:04:47 -0300

    atualizacao aulas

--- Aulas/40_CalculadoraParte1.py
+++
@@ -1,8 +0,0 @@
-""" Calculadora com while """
-while True:
-    print('nummmmm')
-    #########
-    sair = input('Quer sair? [s]im: ').lower().startswith('s')
-
-    if sair is True:
-        break
